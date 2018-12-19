e9a9c4045d4e47ea592fb270293e320f7f5c9a96
VieZhong <zyw13579@qq.com>
2018-12-19 22:09:28 +0800

    speed up

--- make_datafiles_en.py
+++ make_datafiles_en.py
@@ -293,8 +293,8 @@ if __name__ == '__main__':
 
   # Read the tokenized stories, do a little postprocessing then write to bin files
   write_to_bin(stories["testing"], os.path.join(finished_files_dir, "test.bin"))
-  write_to_bin(stories["validation"], os.path.join(finished_files_dir, "val.bin"))
-  write_to_bin(stories["training"], os.path.join(finished_files_dir, "train.bin"), makevocab=True)
+  # write_to_bin(stories["validation"], os.path.join(finished_files_dir, "val.bin"))
+  # write_to_bin(stories["training"], os.path.join(finished_files_dir, "train.bin"), makevocab=True)
 
   # Chunk the data. This splits each of train.bin, val.bin and test.bin into smaller chunks, each containing e.g. 1000 examples, and saves them in finished_files/chunks
   chunk_all(finished_files_dir)
